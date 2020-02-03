34b8f4f88fddad2ee8b6531ae3ab1f3c5a270067
vinothkumar <vinothkumar.suppaiya@ltts.com>
2020-02-02 19:34:32 -1200

    completion

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article <ApplicationRecord
+	belongs_to:user
+	has_many :article_categories
+	has_many :categories, through: :article_categories
+	validates:title, presence:true, length:{minimum:3,maximum:10}
+	validates:description,presence:true, length:{minimum:3,maximum:1000}
+
+end
---
+++ db/migrate/20200120081726_create_article.rb
@@ -0,0 +1,8 @@
+class CreateArticle < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+    	t.string:title
+    
+    end
+  end
+end
